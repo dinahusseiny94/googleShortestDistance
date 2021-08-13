01b52cca61371661af8a81d8d82a493210f6368a
dinaelhusseiny <dinahusseiny15@gmail.com>
2021-08-13 11:51:18 +0200

    intial load

---
+++ ShortestDistance/PathCalculator.py
@@ -0,0 +1,52 @@
+from time import sleep
+
+
+def find(chrome,source_location,destination,sourceLocation,targetLocation,shortestRouteTitle,shortestRouteDistance):
+    sleep(2)
+    chrome.get("https://www.google.com/maps/dir/" + source_location)
+    minDistance = 10000
+    minIndex = 0
+    routeTitleCol = []
+    sleep(5)
+    targetLocationInput = chrome.find_element_by_xpath(
+        '/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div/input')
+    targetLocationInput.send_keys(destination)
+    sleep(5)
+    searchButton = chrome.find_element_by_xpath(
+        '/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/button[1]')
+    searchButton.click()
+    sleep(5)
+    routes = chrome.find_elements_by_class_name('section-directions-trip-title')
+    routes_distances = chrome.find_elements_by_class_name('section-directions-trip-distance')
+    while len(routes) == 0:
+        routes = chrome.find_elements_by_class_name('section-directions-trip-title')
+        routes_distances = chrome.find_elements_by_class_name('section-directions-trip-distance')
+    for routeTitle in routes:
+        routeTitleText = routeTitle.text
+        if routeTitleText != '':
+            routeTitleCol.append(routeTitleText)
+    count = 0
+    for routeDistance in routes_distances:
+        routeDistanceText = routeDistance.text
+        routeDistanceText = routeDistanceText.replace('m', '')
+        routeDistanceText = routeDistanceText.replace('م', '')
+        if 'k' not in routeDistanceText and 'ك' not in routeDistanceText:
+            routeDistanceText = str(float(routeDistanceText.strip()) / 1000)
+        routeDistanceText = routeDistanceText.replace('k', '')
+        routeDistanceInKM = routeDistanceText.replace('ك', '')
+        if routeDistanceInKM == '':
+            routeDistanceInKM = '10000'
+        minRouteDistance = float(routeDistanceInKM.strip())
+        if minRouteDistance < minDistance:
+            minDistance = minRouteDistance
+            minIndex = count
+        count = count + 1
+    sourceLocation.append(source_location)
+    targetLocation.append(destination)
+    shortestRouteDistance.append(minDistance)
+    shortestRouteTitle.append(routeTitleCol[minIndex])
+    dict_result = {"sourceLocation": sourceLocation, "targetLocation": targetLocation,
+                   "shortestRouteDistance": shortestRouteDistance, "shortestRouteTitle": shortestRouteTitle}
+    return dict_result
+
+
---
+++ ShortestDistance/views.py
@@ -0,0 +1,51 @@
+import ShortestDistance.PathCalculator as pathCalc
+import pandas as pd
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import GoogleMaps.settings as stt
+import os
+from selenium import webdriver
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def ShortestPath(request):
+    sourceLocation = []
+    targetLocation = []
+    shortestRouteTitle = []
+    shortestRouteDistance = []
+
+    outputFileName = request.POST["outputFileName"]
+    source_location = request.POST["location"]
+    uploaded_file = request.FILES['document']
+    fs = FileSystemStorage()
+    name = fs.save(uploaded_file.name, uploaded_file)
+    BASE_DIR = stt.BASE_DIR
+    path = os.path.join(BASE_DIR, name)
+    target_locations = pd.read_csv(path)
+    output_df = pd.DataFrame
+
+    chrome = webdriver.Chrome()
+
+    for target_location in target_locations['Target Locations']:
+        output_df = pathCalc.find(chrome, source_location, target_location, sourceLocation, targetLocation
+                                  , shortestRouteTitle,
+                                  shortestRouteDistance)
+    chrome.quit()
+
+    df = pd.DataFrame(
+        {'Source Location': output_df['sourceLocation'],
+         'Target Location': output_df['targetLocation'],
+         'Route Name': output_df['shortestRouteTitle'],
+         'Route Distance in KMs': output_df['shortestRouteDistance']
+         })
+
+    output_filename = outputFileName + '.csv'
+    output_path = os.path.join(BASE_DIR, output_filename)
+
+    df.to_csv(output_path, index=False, header=True, encoding='utf-8-sig')
+    context = {"result": df.to_html(), 'url': output_path}
+    return render(request, 'Distances.html', context)
