bd50a361ff58c72186b84afa6ac760eee18f390a
dinahusseiny <dhusseiny4@gmail.com>
2021-08-14 11:45:32 +0200

    intial load

--- ShortestDistance/views.py
+++ ShortestDistance/views.py
@@ -29,25 +29,25 @@ def ShortestPath(request):
     target_locations = pd.read_csv(path)
     output_df = pd.DataFrame
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-    for target_location in target_locations['Target Locations']:
-        output_df = pathCalc.find(request,chrome, source_location, target_location, sourceLocation, targetLocation
-                                  , shortestRouteTitle,
-                                  shortestRouteDistance)
-    chrome.quit()
-
-    df = pd.DataFrame(
-        {'Source Location': output_df['sourceLocation'],
-         'Target Location': output_df['targetLocation'],
-         'Route Name': output_df['shortestRouteTitle'],
-         'Route Distance in KMs': output_df['shortestRouteDistance']
-         })
-
-    context = {"result": df.to_html()}
-    return render(request, 'Distances.html', context)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    #
+    # for target_location in target_locations['Target Locations']:
+    #     output_df = pathCalc.find(request, chrome, source_location, target_location, sourceLocation, targetLocation
+    #                               , shortestRouteTitle,
+    #                               shortestRouteDistance)
+    # chrome.quit()
+    #
+    # df = pd.DataFrame(
+    #     {'Source Location': output_df['sourceLocation'],
+    #      'Target Location': output_df['targetLocation'],
+    #      'Route Name': output_df['shortestRouteTitle'],
+    #      'Route Distance in KMs': output_df['shortestRouteDistance']
+    #      })
+    #
+    # context = {"result": df.to_html()}
+    return render(request, 'Distances.html', target_locations)
